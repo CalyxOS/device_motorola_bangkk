dd523e80791430cbbe004b349cb66395f9b050cd
Michael Bestas <mkbestas@gmail.com>
2023-04-04 22:10:12 +0300

    Initial CalyxOS makefile
    Change-Id: I6dfcc9b2b5b686df8d2bbe4183bb687777ab576e

---
+++ calyx_bangkk.mk
@@ -0,0 +1,26 @@
+#
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common stuff
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
+
+# Inherit from bangkk device
+$(call inherit-product, device/motorola/bangkk/device.mk)
+
+PRODUCT_NAME := calyx_bangkk
+PRODUCT_DEVICE := bangkk
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g84 5G
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="bangkk_g-user 14 U1TCS34.22-64-19-4-3 eae476-b2a7b2 release-keys MUR1-0.90" \
+    BuildFingerprint=motorola/bangkk_g/bangkk:14/U1TCS34.22-64-19-4-3/eae476-b2a7b2:user/release-keys \
+    DeviceProduct=bangkk_g
